4c7bd766fe9ca0a937ed34b0d08b6b592b5100bb
comblock <81411315+comblock@users.noreply.github.com>
2022-02-23 14:40:33 +0100

    fixed example

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,17 @@
 //! # Example
 //! ```
 //! const CID: &str = "client id from an azure application"
-//!
 //! fn main() {
-//! let client = &reqwest::blocking::Client::new();
+//!    let client = &reqwest::blocking::Client::new();
 //!    let device_code = ms_auth_mc::DeviceCode::new(CID, None, client).unwrap();
+//!    
 //!    if !device_code.cached {
 //!      println!("{}", device_code.message)
+//!    }
+//!    
+//!    let mca = device_code.authenticate(client).unwrap(); // Never use unwrap here, it's used in this example for simplicity
+//!    println!("{:?}", mca)
 //! }
-//!
-//! let mca = device_code.authenticate(client).unwrap(); // Never use unwrap here, it's used in this example for simplicity
-//! println!("{:?}", mca)
 //! ```
 
 use {
